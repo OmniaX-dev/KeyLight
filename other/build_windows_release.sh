28c9933cd704e6f56ff1ab0b3b7e7a3222ca5ed0
OmniaX-Dev <syl4r39@gmail.com>
2025-10-28 02:49:12 +0100

    Consolidated build scripts

---
+++ other/build_windows_release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ..
+./build release
+
+mkdir bin/KeyLight_w64
+
+cp -r extra/* bin/KeyLight_w64/
+cp other/appIcon.ico bin/KeyLight_w64/themes/ui
+
+cp bin/KeyLight.exe bin/KeyLight_w64
+
+for dll in libgcc_s_seh-1.dll libstdc++-6.dll libostd.dll libwinpthread-1.dll \
+           tgui.dll sfml-audio-3.dll sfml-graphics-3.dll sfml-system-3.dll sfml-window-3.dll; do
+    src="C:/msys64/ucrt64/bin/$dll"
+    [[ -f "$src" ]] || { echo "Missing: $dll"; exit 1; }
+    cp "$src" bin/KeyLight_w64/
+done
+
+cp -r licences bin/KeyLight_w64
+cp LICENSE bin/KeyLight_w64/licences/KeyLight-LICENCE.txt
